ff99f1c5925d97139ec91ec765718d903abb8b9c
Kelvin Ochubili <kelvinochubili@gmail.com>
2024-03-14 19:35:38 +0100

    feat: desktop view for home preview center section

--- src/app/(home)/HomePreview/Center.tsx
+++ src/app/(home)/HomePreview/Center.tsx
@@ -1,7 +1,19 @@
+import Link from 'next/link'
+import Buttons from '@/components/Buttons'
+
 import c from '../Home.module.scss'
 
 const Center = () => {
-	return <div className={c['home-preview-center']}></div>
+	return (
+		<div className={c['home-preview-center']}>
+			<div className={c['home-preview-center-left']}>
+				<h3>ZX7 SPEAKER</h3>
+				<Link href='/speakers/zx7'>
+					<Buttons type='secondary'>see product</Buttons>
+				</Link>
+			</div>
+		</div>
+	)
 }
 
 export default Center
