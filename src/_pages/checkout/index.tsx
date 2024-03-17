89d34790ece856fc7077bbfdd1d3a3fefc2afa7f
Kelvin Ochubili <kelvinochubili@gmail.com>
2024-03-17 08:48:50 +0100

    feat: created checkout page

---
+++ src/_pages/checkout/index.tsx
@@ -0,0 +1,7 @@
+import c from './checkout.module.scss'
+
+const Checkout = () => {
+	return <div className={c.checkout}>Checkout</div>
+}
+
+export default Checkout
---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,7 @@
+import Checkout from '@/_pages/checkout'
+
+const CheckoutPage = () => {
+	return <Checkout />
+}
+
+export default CheckoutPage
