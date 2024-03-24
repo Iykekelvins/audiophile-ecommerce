e615aa9e64382bba3bde05531d46d296949c761e
Kelvin Ochubili <kelvinochubili@gmail.com>
2024-03-24 14:36:27 +0100

    feat: mobile menu animation added

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,84 @@
 'use client'
 
+import { useEffect, useRef, useState } from 'react'
 import { usePathname } from 'next/navigation'
 import { navLinks } from '@/utils/mock'
 import { animateCart } from '@/utils/animations'
 import { useRouter } from 'next/navigation'
+import { useGSAP } from '@gsap/react'
 
 import Link from 'next/link'
 import Image from 'next/image'
+import gsap from 'gsap'
 import cn from '@/utils/cn'
 
 import c from './Navbar.module.scss'
 
 const Navbar = () => {
 	const pathname = usePathname()
-
 	const router = useRouter()
+
+	const menuref = useRef(null)
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen)
+	}
+
+	const tl = useRef<any>(null)
+
+	useGSAP(() => {
+		gsap.set('.link-holder', {
+			y: 75,
+		})
+
+		tl.current = gsap
+			.timeline({ paused: true })
+
+			.to(menuref.current, {
+				clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+				ease: 'power4.inOut',
+				duration: 1.25,
+			})
+			.to(
+				'.link-holder',
+				{
+					y: 0,
+					duration: 1,
+					stagger: 0.1,
+					ease: 'power4.inOut',
+				},
+				'-=0.95'
+			)
+			.to(
+				'.line-1',
+				{
+					display: 'none',
+					duration: 0.1,
+					ease: 'power4',
+				},
+				'-=0.5'
+			)
+			.to(
+				'.line-2',
+				{
+					rotate: '45deg',
+					duration: 0.1,
+					ease: 'power4',
+				},
+				'-=0.5'
+			)
+			.to(
+				'.line-3',
+				{
+					rotate: '-45deg',
+					marginTop: '-7px',
+					duration: 0.1,
+					ease: 'power4',
+				},
+				'-=0.5'
+			)
+	})
 
 	const handleCloseCart = (href: string) => {
 		const cart: HTMLDivElement | null = document.querySelector('.cart')
@@ -30,21 +94,22 @@ const Navbar = () => {
 		}
 	}
 
+	useEffect(() => {
+		if (isMenuOpen) {
+			tl.current.play()
+		} else {
+			tl.current.reverse()
+		}
+	}, [isMenuOpen])
+
 	return (
 		<nav className={cn(c.navbar, pathname !== '/' ? c.black : '')}>
 			<div className={c['navbar-inner']}>
 				<div className={c['navbar-inner-left']}>
-					<button>
-						<svg
-							width='16'
-							height='15'
-							viewBox='0 0 16 15'
-							fill='none'
-							xmlns='http://www.w3.org/2000/svg'>
-							<rect width='16' height='3' fill='white' />
-							<rect y='6' width='16' height='3' fill='white' />
-							<rect y='12' width='16' height='3' fill='white' />
-						</svg>
+					<button onClick={toggleMenu} className={c.toggle}>
+						<span className='line-1'></span>
+						<span className='line-2'></span>
+						<span className='line-3'></span>
 					</button>
 
 					<Link href='/' onClick={() => handleCloseCart('/')}>
@@ -63,6 +128,23 @@ const Navbar = () => {
 				<button onClick={() => animateCart()}>
 					<Image src='/icons/cart.svg' height={20} width={23} alt='cart icon' />
 				</button>
+			</div>
+
+			<div className={`${c['navbar-mobile']} mobile-menu`} ref={menuref}>
+				<div className={c['navbar-mobile-links']}>
+					{navLinks.map((link) => (
+						<div className={c['navbar-mobile-links-item']} key={link.name}>
+							<div
+								className={cn(c['navbar-mobile-links-item-holder'], 'link-holder')}
+								onClick={toggleMenu}>
+								<Link href={link.href}>{link.name}</Link>
+							</div>
+						</div>
+					))}
+				</div>
+				<p className={c.copyright}>
+					Copyright {new Date().getFullYear()}. All Rights Reserved
+				</p>
 			</div>
 		</nav>
 	)
