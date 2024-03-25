66f4596fa60200b095c7fcc8a08098de03c31296
Kelvin Ochubili <kelvinochubili@gmail.com>
2024-03-25 18:35:25 +0100

    feat: scroll to top of page on checkout error

--- src/_pages/checkout/Summary.tsx
+++ src/_pages/checkout/Summary.tsx
@@ -48,6 +48,11 @@ const Summary = () => {
 			setCheckoutErrors(true)
 			setLoading(false)
 
+			const isBrowser = () => typeof window !== 'undefined' //The approach recommended by Next.js
+
+			if (!isBrowser()) return
+			window.scrollTo({ top: 0, behavior: 'smooth' })
+
 			return
 		}
 
